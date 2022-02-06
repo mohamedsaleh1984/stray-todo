b7a55bb3a9301ae766dfb1e72760af1ae29252a1
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2022-02-06 14:07:20 -0500

    Add project files.

---
+++ StrayToDo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Linq;
+
+
+
+namespace StrayToDo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> extensions = new List<string>() { "cpp", "h", "txt", "cs" };
+            List<string> files = getAllFiles(location, extensions);
+            files = findFilesWithToDoTag(files);
+            foreach (var file in files)
+                Console.WriteLine(file); 
+            Console.ReadLine();
+        }
+    }
+}
